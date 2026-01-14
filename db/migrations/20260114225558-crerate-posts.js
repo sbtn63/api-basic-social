c525cf3c7af1cb683a65a38604c01577cc480365
rei <rei@localhost.localdomain>
2026-01-14 18:01:22 -0500

    Configurando migraciones #8

--- db/migrations/20260114225558-crerate-posts.js
+++ db/migrations/20260114225558-crerate-posts.js
@@ -1,22 +1,14 @@
 'use strict';
 
+const {PostSchema, POST_TABLE} = require('../models/post.models');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface) {
+    await queryInterface.createTable(POST_TABLE, PostSchema);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    await queryInterface.dropTable(POST_TABLE);
   }
 };
--- db/migrations/20260114225606-crerate-comments.js
+++ db/migrations/20260114225606-crerate-comments.js
@@ -1,22 +1,14 @@
 'use strict';
 
+const { CommentSchema, COMMENT_TABLE } = require("../models/comment.models");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface) {
+    await queryInterface.createTable(COMMENT_TABLE, CommentSchema);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    await queryInterface.dropTable(COMMENT_TABLE);
   }
 };
