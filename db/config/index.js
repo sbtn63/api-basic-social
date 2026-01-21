3cb9dae566adf9003b0f0ebcb1bb3785e4ddb52b
rei <rei@localhost.localdomain>
2026-01-21 18:27:06 -0500

    Agregando configuracion ddbb render #35

--- db/config/index.js
+++ db/config/index.js
@@ -16,5 +16,19 @@ module.exports = {
     database: config.dbName,
     port: config.dbPort,
     dialect: config.dbDialect
+  },
+  qa: {
+    host: config.dbHost,
+    username: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    port: config.dbPort,
+    dialect: config.dbDialect,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
