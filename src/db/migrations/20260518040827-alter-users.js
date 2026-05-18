643087660c9ef15b54d762e335c6346f8a49dca3
Sebastián Laverde <151546685+sbtn63@users.noreply.github.com>
2026-05-17 23:42:21 -0500

    #159 change migration pipeline pass

--- src/db/migrations/20260518040827-alter-users.js
+++ src/db/migrations/20260518040827-alter-users.js
@@ -3,18 +3,30 @@
 import { USER_TABLE } from '../models/user.models.js';
 
 export async function up (queryInterface, Sequelize) {
-   await queryInterface.addColumn(USER_TABLE, 'is_verified', {
+  const tableDefinition = await queryInterface.describeTable(USER_TABLE);
+
+  if (!tableDefinition.is_verified) {
+    await queryInterface.addColumn(USER_TABLE, 'is_verified', {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false,
     });
+  }
 
+  if (!tableDefinition.email_verified_at) {
     await queryInterface.addColumn(USER_TABLE, 'email_verified_at', {
       type: Sequelize.DATE
     });
-};
+  }
+}
 
 export async function down (queryInterface) {
+  const tableDefinition = await queryInterface.describeTable(USER_TABLE);
+
+  if (tableDefinition.is_verified) {
     await queryInterface.removeColumn(USER_TABLE, 'is_verified');
+  }
+  if (tableDefinition.email_verified_at) {
     await queryInterface.removeColumn(USER_TABLE, 'email_verified_at');
-};
+  }
+}
